6890307a4cad7d95ce4d29e2804e82afbc902296
Prinson T <you@example.com>
2025-01-21 21:13:58 +0530

    edit

--- methods/src/NormalMethods.java
+++ methods/src/NormalMethods.java
@@ -1,10 +1,12 @@
 public class NormalMethods {
     public void code(int a,int b){
         System.out.println(a+b);
+
     }
     public static void main(String[] args) {
         NormalMethods m=new NormalMethods();
         m.code(5,8);
+        m.code(5,1);
         
     }
 }
